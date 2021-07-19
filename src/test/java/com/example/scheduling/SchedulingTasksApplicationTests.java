525d9aa57ea00b9b782c3978932bbd062af7a38f
prakashsharma <prakash3210@gmail.com>
2021-07-19 18:18:57 +0530

    C3

--- src/test/java/com/example/scheduling/SchedulingTasksApplicationTests.java
+++ src/test/java/com/example/scheduling/SchedulingTasksApplicationTests.java
@@ -3,14 +3,13 @@ package com.example.scheduling;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SchedulingTasksApplicationTests {
 
-	@SpyBean
+	@Autowired
 	private ScheduledTask tasks;
 
 	@Test
