7834a015e743118bd88f63b79f94d17d36693e5e
prakashsharma <prakash3210@gmail.com>
2021-07-19 16:50:25 +0530

    C2

--- src/main/java/com/example/scheduling/ScheduledTask.java
+++ src/main/java/com/example/scheduling/ScheduledTask.java
@@ -6,8 +6,9 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class ScheduledTask {
 	
 	private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
